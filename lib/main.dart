c4606645fbb43433bc199ab9de591dd574775a5d
Joscha von Andrian <joschavonandrian@gmail.com>
2022-01-09 00:15:19 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:fixmymaze/pages/home/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
