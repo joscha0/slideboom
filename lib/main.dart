0cf7f263ebb557a37b0f256aa5a3a1548301636a
Joscha von Andrian <joschavonandrian@gmail.com>
2022-01-09 19:04:26 +0100

    rename files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:fixmymaze/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import 'pages/game/game_page.dart';
 
 void main() {
   runApp(const GetMaterialApp(
--- lib/pages/game/game_page.dart
+++ lib/pages/game/game_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'home_controller.dart';
+import 'game_controller.dart';
 
 class HomePage extends GetView<HomeController> {
   const HomePage({Key? key}) : super(key: key);
