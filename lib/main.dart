87c7220da8b91dacc02a4df01da63c7edaa2fcd8
Joscha von Andrian <joschavonandrian@gmail.com>
2022-02-05 22:51:46 +0100

    add get storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'pages/home/home_page.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
+
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData.light().copyWith(
