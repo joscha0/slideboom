4cca9213ca13d2b6229a3c79da984383eaeb1222
Joscha von Andrian <joschavonandrian@gmail.com>
2022-02-06 03:15:16 +0100

    make box private

--- lib/storage/scores.dart
+++ lib/storage/scores.dart
@@ -1,17 +1,17 @@
 import 'package:get_storage/get_storage.dart';
 
-final box = GetStorage();
+final _box = GetStorage();
 
 getScores(String mode, bool bombs) {
   String id = "scores-" + mode + (bombs ? "b" : "");
-  List scores = box.read(id) ?? [];
+  List scores = _box.read(id) ?? [];
   return scores;
 }
 
 addScore(int rowCount, bool bombs, int time) {
   // id: scores-4x4b
   String id = "scores-${rowCount}x$rowCount" + (bombs ? "b" : "");
-  List scores = box.read(id) ?? [];
+  List scores = _box.read(id) ?? [];
   Map slowest = {'time': 0};
   if (scores.length > 1) {
     slowest = scores.reduce((a, b) => a['time'] > b['time'] ? a : b);
@@ -30,5 +30,5 @@ _saveScore(String id, int time, List scores) {
     'date': now.toString(),
     'time': time,
   });
-  box.write(id, scores);
+  _box.write(id, scores);
 }
