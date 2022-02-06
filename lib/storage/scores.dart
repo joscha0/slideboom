3a6dced22e35fea79ae137282aa4889110095e0f
Joscha von Andrian <joschavonandrian@gmail.com>
2022-02-06 15:43:22 +0100

    add high score

--- lib/storage/scores.dart
+++ lib/storage/scores.dart
@@ -8,20 +8,34 @@ getScores(String mode, bool bombs) {
   return scores;
 }
 
-addScore(int rowCount, bool bombs, int time) {
+bool addScore(int rowCount, bool bombs, int time) {
+  /// returns true if score is new highscore
   // id: scores-4x4b
   String id = "scores-${rowCount}x$rowCount" + (bombs ? "b" : "");
   List scores = _box.read(id) ?? [];
   Map slowest = {'time': 0};
+  bool isHishscore = false;
   if (scores.length > 1) {
     slowest = scores.reduce((a, b) => a['time'] > b['time'] ? a : b);
+    Map fastest = scores.reduce((a, b) => a['time'] < b['time'] ? a : b);
+    if (fastest['time'] > time) {
+      isHishscore = true;
+    }
+  } else if (scores.isNotEmpty) {
+    if (scores[0]['time'] > time) {
+      isHishscore = true;
+    }
+  } else {
+    isHishscore = true;
   }
+
   if (scores.length < 10) {
     _saveScore(id, time, scores);
   } else if (slowest['time'] > time) {
     scores.removeWhere((score) => score == slowest);
     _saveScore(id, time, scores);
   }
+  return isHishscore;
 }
 
 _saveScore(String id, int time, List scores) {
