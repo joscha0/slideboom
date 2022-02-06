f9dbcc22cec4adc84330fb6b3ff978685911acba
Joscha von Andrian <joschavonandrian@gmail.com>
2022-02-06 16:23:54 +0100

    add responsive framework

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'package:slideboom/routes/app_pages.dart';
 
 void main() async {
@@ -9,7 +10,16 @@ void main() async {
 
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
+    builder: (context, widget) => ResponsiveWrapper.builder(
+      ClampingScrollWrapper.builder(context, widget!),
+      breakpoints: const [
+        ResponsiveBreakpoint.resize(350, name: MOBILE),
+        ResponsiveBreakpoint.autoScale(600, name: TABLET),
+        ResponsiveBreakpoint.resize(800, name: DESKTOP),
+      ],
+    ),
     theme: ThemeData.light().copyWith(
+      scaffoldBackgroundColor: Colors.indigo[100],
       textTheme: GoogleFonts.bungeeTextTheme(),
     ),
     getPages: AppPages.routes,
