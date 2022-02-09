af22b1bae62abdd1c03685d31fcb3046e0be9ec6
Joscha von Andrian <joschavonandrian@gmail.com>
2022-02-09 22:48:51 +0100

    flutter add lottie animation

--- android/app/src/main/kotlin/com/example/slideboom/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/slideboom/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.slideboom
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.SplashScreen
 
 class MainActivity: FlutterActivity() {
+
+override fun provideSplashScreen(): SplashScreen? = SplashView()
 }
+
