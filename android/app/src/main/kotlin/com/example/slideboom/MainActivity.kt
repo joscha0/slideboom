52b8d059f9b2d6511f275d4d463a8bd05b0f7137
Joscha von Andrian <joschavonandrian@gmail.com>
2022-02-18 18:31:17 +0100

    revert android to native splash

--- android/app/src/main/kotlin/com/example/slideboom/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/slideboom/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.slideboom
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.SplashScreen
 
 class MainActivity: FlutterActivity() {
-
-override fun provideSplashScreen(): SplashScreen? = SplashView()
 }
 
