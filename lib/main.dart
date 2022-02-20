fb81e3883acf3c61288174b3cbf3e4422fde506c
Joscha <joschavonandrian@gmail.com>
2022-02-20 02:32:00 +0100

    add custom window border

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:slideboom/app.dart';
@@ -6,4 +7,13 @@ void main() async {
   await GetStorage.init();
 
   runApp(const MyApp());
+  doWhenWindowReady(() {
+    final win = appWindow;
+    const initialSize = Size(600, 450);
+    win.minSize = initialSize;
+    win.size = initialSize;
+    win.alignment = Alignment.center;
+    win.title = "flutter";
+    win.show();
+  });
 }
